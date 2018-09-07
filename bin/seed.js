3248224748f92882501637f0e80059e2b0b99e5c
abisummers <hello@abisummers.com>
2018-09-07 15:07:10 +0200

    added object

--- bin/seed.js
+++ bin/seed.js
@@ -4,12 +4,17 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user-model.js");
 
 mongoose
-  .connect('mongodb://localhost/express-users', {useNewUrlParser: true})
+  .connect(
+    "mongodb://localhost/express-users",
+    { useNewUrlParser: true }
+  )
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error("Error connecting to mongo", err);
   });
 
 const userData = [
@@ -17,22 +22,27 @@ const userData = [
     fullName: "Greg Gardener",
     email: "g@g.g",
     encryptedPassword: bcrypt.hashSync("ggggg0", 10),
-    role: "admin",
+    role: "admin"
   },
   {
     fullName: "ABy Gardener",
     email: "ghdj@us.g",
     encryptedPassword: bcrypt.hashSync("ggggg0", 10),
-    role: "admin",
+    role: "admin"
   },
   {
     fullName: "Barnabus Bixley",
     email: "b@b.b",
     encryptedPassword: bcrypt.hashSync("bbbbb0", 10),
-    role: "admin",
+    role: "admin"
+  },
+  {
+    fullName: "Sophie Clarke",
+    email: "S@s.s",
+    encryptedPassword: bcrypt.hashSync("sssss0", 10),
+    role: "normal"
   }
 ];
-
 
 User.create(userData)
   .then(userResults => {
